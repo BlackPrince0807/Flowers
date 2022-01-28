4be2f1af24200609f8520bce3171f01b83695428
Avazbek <foziljonovavazbek0807@gmail.com>
2022-01-28 17:38:44 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// swiper carusel
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
